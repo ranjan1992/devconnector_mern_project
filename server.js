d13ec0c5983ef449eb326429535909a8b996f3d1
Ranjan Verma <verma.ranjan62@gmail.com>
2025-12-17 16:32:47 +0530

    Route files with Express Router

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
